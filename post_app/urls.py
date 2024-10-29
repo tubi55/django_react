bdccb814deada565686586458ad582dea662a7f2
tubi55 <tubi55@nate.com>
2024-10-29 10:51:22 +0900

    상세페이지 전용 url, views매핑

--- post_app/urls.py
+++ post_app/urls.py
@@ -4,5 +4,6 @@ from .import views
 # 순서3 - 브라우저에서 들어온 url패턴을 확인해서 views.py에 있는 함수 호출
 # 주소창에 localhost:8000/posts라는 요청이 들어오면 아래 url패턴이 인지해서 views.py에 있는 posts 함수 실행
 urlpatterns = [
-  path('posts', views.posts, name='posts')
+  path('posts', views.posts, name='posts'),
+  path('posts/<slug:slug>/', views.posts_detail, name='post-detail')
 ]
